1e40444715fe515be17058a69d65da0150384a1e
Superior5 <superiorunknown5@gmail.com>
2022-05-23 17:35:59 +0300

    Изменил цвет партиклов

--- js/particles.js
+++ js/particles.js
@@ -154,7 +154,7 @@ console.log(33434);
           ctx.beginPath();
           ctx.moveTo(p.x, p.y);
           ctx.lineTo(p.closest[i].x, p.closest[i].y);
-          ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+          ctx.strokeStyle = 'rgba(255, 199, 91,'+ p.active+')';
           ctx.stroke();
       }
   }
@@ -173,7 +173,7 @@ console.log(33434);
           if(!_this.active) return;
           ctx.beginPath();
           ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-          ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+          ctx.fillStyle = 'rgba(255, 199, 91,'+ _this.active+')';
           ctx.fill();
       };
   }
